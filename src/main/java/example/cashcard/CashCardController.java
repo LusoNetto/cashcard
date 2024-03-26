9bb796d17d7705b7dd0fe6140f456364e8ff1a6c
LusoNetto <luciano.silva137@hotmail.com>
2024-03-26 18:08:44 -0300

    add: repository and H2 database

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,5 +1,7 @@
 package example.cashcard;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,13 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cashcards")
 class CashCardController {
 
+	private final CashCardRepository cashCardRepository;
+
+	private CashCardController(CashCardRepository cashCardRepository) {
+		this.cashCardRepository = cashCardRepository;
+	}
+
 	@GetMapping("/{requestedId}")
 	private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-	    if (requestedId.equals(99L)) {
-	        CashCard cashCard = new CashCard(99L, 123.45);
-	        return ResponseEntity.ok(cashCard);
-	    } else {
-	        return ResponseEntity.notFound().build();
-	    }
+		Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+		if (cashCardOptional.isPresent()) {
+			return ResponseEntity.ok(cashCardOptional.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+
 	}
 }
