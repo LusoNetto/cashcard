0479be6974256020bde8abb85b1c5bd7cd7cc905
LusoNetto <luciano.silva137@hotmail.com>
2024-03-27 17:07:28 -0300

    add: implementando post no cashCard e adicionando os testes

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -1,7 +1,9 @@
 package example.cashcard;
 
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +11,8 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
@@ -36,5 +39,23 @@ class CashCardApplicationTests {
 
       assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
       assertThat(response.getBody()).isBlank();
+    }
+    
+    @Test
+    void shouldCreateANewCashCard() {
+       CashCard newCashCard = new CashCard(null, 250.00);
+       ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+       assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+       URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+       ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+       assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+       DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+       Number id = documentContext.read("$.id");
+       Double amount = documentContext.read("$.amount");
+
+       assertThat(id).isNotNull();
+       assertThat(amount).isEqualTo(250.00);
     }
 }
