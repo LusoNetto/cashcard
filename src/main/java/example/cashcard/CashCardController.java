e867d51623dfb7fadea4caa42bdce71612384d46
LusoNetto <luciano.silva137@hotmail.com>
2024-03-30 16:25:25 -0300

    add: getall com paginação e ordenação

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,8 +1,13 @@
 package example.cashcard;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +36,17 @@ class CashCardController {
 			return ResponseEntity.notFound().build();
 		}
 
+	}
+	
+	@GetMapping
+	private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+	    Page<CashCard> page = cashCardRepository.findAll(
+	            PageRequest.of(
+	                    pageable.getPageNumber(),
+	                    pageable.getPageSize(),
+	                    pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+	    ));
+	    return ResponseEntity.ok(page.getContent());
 	}
 
 	@PostMapping
