4464902f52972bb71f2503903febfe4e1e841846
unknown <alunos@015LAB1219.umc.br>
2024-04-01 18:50:25 -0300

    add: spring secyrity on project and configuration class

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -48,7 +48,7 @@ class CashCardApplicationTests {
     @Test
     @DirtiesContext
     void shouldCreateANewCashCard() {
-       CashCard newCashCard = new CashCard(null, 250.00);
+       CashCard newCashCard = new CashCard(null, 250.00, "sarah1");
        ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
